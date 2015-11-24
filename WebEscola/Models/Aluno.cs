5646fd3cf539d64d3135108abfb43bb6b7064d99
Aron Rodrigues <aronrodrigues@gmail.com>
2015-11-24 16:23:23 -0200

    Initial revision

---
+++ WebEscola/Models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebEscola.Models
+{
+    public class Aluno
+    {
+
+        [Required(ErrorMessage="É necessário informar um {0}")]
+        [Range(1000, 9999, ErrorMessage="O campo {0} deve estar entre {1} e {2}")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Digite número válido para o {0}.")]
+        [DisplayName("Matrícula")]
+        public Int16 ID { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar um {0}")]
+        [StringLength(50, MinimumLength=3, ErrorMessage="O campo {0} deve ter entre {1} e {2} caracteres")]
+        public String Nome { get; set; }
+
+    }
+}
