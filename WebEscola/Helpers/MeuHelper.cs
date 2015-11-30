f143d8be5f5170e46cd348a171b0d8c85008ab19
Aron Rodrigues <aronrodrigues@gmail.com>
2015-11-30 14:58:11 -0200

    Implementado custom helper para truncar textos.

---
+++ WebEscola/Helpers/MeuHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebEscola.Helpers
+{
+    public static class MeusHelpers
+    {
+        public static string TextoTrucadoFor<TModel, TValue>(
+                this HtmlHelper<TModel> htmlHelper, 
+                Expression<Func<TModel, TValue>> expression, 
+                int tamanho)
+        {
+            var metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            var value = metadata.Model;
+            if ( value.ToString().Length <= tamanho)
+            {
+                return value.ToString();
+            }
+            else
+            {
+                return value.ToString().Substring(0, tamanho) + "...";
+            }
+        }
+
+    }
+}
